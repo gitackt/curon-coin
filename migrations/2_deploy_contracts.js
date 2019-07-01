ff69f605e087916da66b52983a2bda4c8e99c751
Takuto Mitsuhashi <picos.tackt@gmail.com>
2019-07-01 21:05:53 +0900

    Fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,15 +2,15 @@ const CuronCoin = artifacts.require("./CuronCoin.sol");
 const CuronCoinSale = artifacts.require("./CuronCoinSale.sol");
 
 module.exports = function(deployer) {
-  const issueAddress = web3.eth.getAccounts()[0];
+  let issueAddress = null;
+  web3.eth.getAccounts().then((e) => issueAddress = e[0])
   const initialSupply = web3.utils.toBN(10*(10**18));
   const rateForWei = web3.utils.toBN(1);
 
-  return deployer.then(() => deployer.deploy(CuronCoin, initialSupply)).then(() => {
-    return deployer.deploy(CuronCoinSale, rateForWei, issueAddress, CuronCoin.address);
-  }).then(() => {
-    return CuronCoin.deployed().then(instance => {
-      instance.transfer(CuronCoinSale.address, initialSupply, { from: issueAddress })
-    });
-  })
+  return deployer
+    .then(() => deployer.deploy(CuronCoin, initialSupply))
+    .then(() => deployer.deploy(CuronCoinSale, rateForWei, issueAddress, CuronCoin.address))
+    .then(() => CuronCoin.deployed()
+      .then(instance => instance.transfer(CuronCoinSale.address, initialSupply, { from: issueAddress }))
+    )
 };
