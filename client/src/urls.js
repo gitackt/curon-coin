6db14970004dc2b7ca39a639e68781c5565c632f
Takuto Mitsuhashi <picos.tackt@gmail.com>
2019-07-10 23:39:00 +0900

    Fix provider node url

--- client/src/urls.js
+++ client/src/urls.js
@@ -1,3 +1,2 @@
-export const nodeUrlDev = "http://127.0.0.1:8545";
-export const nodeUrlProd = "https://mainnet.infura.io";
-export const ethPriceUrl = "https://api.coinmarketcap.com/v2/ticker/1027/?convert=JPY";
+export const nodeUrl = "http://127.0.0.1:8545";
+// export const nodeUrl = "https://mainnet.infura.io";
